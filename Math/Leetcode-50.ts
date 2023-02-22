655b3e710d5eac9cefb971c80d63c1acf3b7dc4b
LeeMir <manofgodmj@gmail.com>
2023-02-22 12:37:39 +0900

    230222 Leetcode-50 재풀이

--- Math/Leetcode-50.ts
+++ Math/Leetcode-50.ts
@@ -1,3 +1,18 @@
 // https://leetcode.com/problems/powx-n
 
-const myPow = (x: number, n: number): number => x ** n;
+// return x ** n;
+// return Math.pow(x, n);
+
+const myPow = (x: number, n: number): number => {
+  if (n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+  if (n % 2 === 0) {
+    return myPow(x * x, n / 2);
+  }
+  return myPow(x * x, Math.trunc(n / 2)) * x;
+};
