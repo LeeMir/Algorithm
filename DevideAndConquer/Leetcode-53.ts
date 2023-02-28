86792ef809f033247b02a128949e512200f369ef
LeeMir <manofgodmj@gmail.com>
2023-02-28 13:44:09 +0900

    230228 Leetcode-53 재풀이
    문제 의도에 맞게 재풀이
    그러나 성능은 좋지 않았음

--- DevideAndConquer/Leetcode-53.ts
+++ DevideAndConquer/Leetcode-53.ts
@@ -1,14 +1,38 @@
 // https://leetcode.com/problems/maximum-subarray
 
 const maxSubArray = (nums: number[]): number => {
-  let tmp = nums[0];
-  let max = nums[0];
-  const size = nums.length;
+  const conquerMax = (l: number, mid: number, r: number) => {
+    let tmp = 0,
+      maxL = -10000,
+      maxR = -10000;
+    for (let i = mid; i >= l; i--) {
+      tmp += nums[i];
+      maxL = Math.max(tmp, maxL);
+    }
 
-  for (let i = 1; i < size; i++) {
-    tmp = Math.max(nums[i], tmp + nums[i]);
-    max = Math.max(max, tmp);
-  }
+    tmp = 0;
 
-  return max;
+    for (let i = mid + 1; i <= r; i++) {
+      tmp += nums[i];
+      maxR = Math.max(tmp, maxR);
+    }
+
+    return maxL + maxR;
+  };
+
+  const devNCon = (l: number, r: number) => {
+    if (l === r) {
+      return nums[l];
+    }
+
+    const mid = ~~((l + r) / 2);
+
+    const maxL = devNCon(l, mid);
+    const maxR = devNCon(mid + 1, r);
+    const maxC = conquerMax(l, mid, r);
+
+    return Math.max(maxL, maxR, maxC);
+  };
+
+  return devNCon(0, nums.length - 1);
 };
